1be76682cb0f64fee384e32cda6fb0ea24a34fd4
khrome <abbey@khrome.net>
2024-10-04 22:29:25 -0600

    alphav4

--- src/index.mjs
+++ src/index.mjs
@@ -7,7 +7,77 @@ if(typeof require !== 'undefined') internalRequire = require;
 const ensureRequire = ()=> (!internalRequire) && (internalRequire = mod.createRequire(import.meta.url));
 //*/
 
-/**
- * A JSON object
- * @typedef { object } JSON
- */
+import { Context } from '@ansi-art/async-context';
+import { Grid } from '@ansi-art/tools';
+import { FigletFont } from '@ansi-art/font';
+import { sync as kitchenSync } from 'kitchen-sync';
+
+const fonts = {};
+
+let baseDir = process.cwd();
+export const setBaseDirectory = (dir)=>{
+    baseDir = dir;
+};
+
+const createAsciiContext = (startState, bitDepth=4)=>{
+    // An AnsiColorContext at the appropriate bit depth
+    //const acc = new Ansi(new Color(bitDepth));
+    // function to convert values to their corresponding codes
+    //const code = (theCode)=> ansi.codeRender([theCode]);
+    // this is the context where the current state of the output is stored
+    // as we step down the render pipeline
+    const textBuffer = new Grid(startState, {bitDepth});
+    let asciiPrototype = {
+        // chain operations
+        identity : (item, cb)=>{ 
+            const callback = kitchenSync(cb);
+            setTimeout(()=>{ callback(null, textBuffer.toString()); }, 100);
+            return callback.return;
+        },
+        font : (item, cb)=>{
+            const callback = kitchenSync(cb);
+            const caselessName = item.figlet.toLowerCase();
+            try{
+                if(!fonts[caselessName]){
+                    const font = new FigletFont(item.figlet, item.path);
+                    fonts[caselessName] = font;
+                    font.loaded.then(()=>{
+                        const letters = fonts[caselessName].write(item.text);
+                        setTimeout(()=>{
+                            callback(null, letters);
+                        });
+                    });
+                }else{
+                    const letters = fonts[caselessName].write(item.text);
+                    setTimeout(()=>{
+                        callback(null, letters);
+                    });
+                }
+            }catch(ex){
+                callback(ex);
+            }
+            return callback.return;
+        }
+    };
+    return asciiPrototype;
+};
+//const AnsiArt = Context.wrap(createAsciiContext());
+//export AnsiArt;
+
+const Art = function(startState='', bitDepth=4){
+    return Context.wrap(createAsciiContext(startState));
+};
+
+Art.loadFigletDirectory = async (path)=>{
+    
+};
+
+Art.loadFigletFont = async (name, path=baseDir)=>{
+    const caselessName = name.toLowerCase();
+    if(!fonts[caselessName]){
+        const font = new FigletFont(name, path);
+        await font.ready;
+    }
+};
+export { Art };
+ 
--- test/test.mjs
+++ test/test.mjs
@@ -1,13 +1,52 @@
 /* global describe:false */
 import { chai } from '@environment-safe/chai';
 import { it } from '@open-automaton/moka';
-import { } from '../src/index.mjs';
+import { Ansi } from '@ansi-art/tools';
+import { Art } from '../src/index.mjs';
+import { Path } from '@environment-safe/file';
 const should = chai.should();
+const ansi = new Ansi();
+
+const basePath = Path.join(
+    Path.current,
+    'test/fonts'
+);
+
+const fooInFigletDoom = `  __               
+ / _|              
+| |_   ___    ___  
+|  _| / _ \\  / _ \\ 
+| |  | (_) || (_) |
+|_|   \\___/  \\___/ 
+                   
+                   
+`;
 
 describe('module', ()=>{
-    describe('performs a simple test suite', ()=>{
-        it('loads', async ()=>{
-            should.exist({});
+    describe('AnsiArt', ()=>{
+        it('performs an identity', async ()=>{
+            should.exist(Art);
+            const emoticons = ':)\n:(';
+            const art = new Art(emoticons);
+            const identity = art.identity({});
+            const promise = identity.promise;
+            const output = await promise;
+            ansi.strip(output).trim().should.equal(emoticons.trim());
+        });
+        
+        it('renders a figlet font', async ()=>{
+            should.exist(Art);
+            const art = new Art();
+            const font = art.font({
+                figlet: 'doom',
+                path: basePath,
+                text: 'foo'
+            });
+            const promise = font.promise;
+            const output = await promise;
+            console.log(fooInFigletDoom);
+            console.log(output);
+            output.should.equal(fooInFigletDoom);
         });
     });
 });
